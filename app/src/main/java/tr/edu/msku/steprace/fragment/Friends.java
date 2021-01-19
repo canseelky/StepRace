e48c58a07125395f1bf6701ebb18941d5ead7943
canselkucukyilmaz <canselhky@gmail.com>
2021-01-20 00:30:41 +0300

    Intent service updated

--- app/src/main/java/tr/edu/msku/steprace/fragment/Friends.java
+++ app/src/main/java/tr/edu/msku/steprace/fragment/Friends.java
@@ -1,34 +1,42 @@
 package tr.edu.msku.steprace.fragment;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.List;
-
 import tr.edu.msku.steprace.adapter.FriendsAdapter;
 import tr.edu.msku.steprace.R;
 import tr.edu.msku.steprace.model.Friend;
+import tr.edu.msku.steprace.model.User;
 
 
 public class Friends extends Fragment {
-
-
     private RecyclerView recyclerView;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mDatabaseReference;
-    private ChildEventListener mChildEventListener;
+    private FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+    private String user_id = mFirebaseAuth.getCurrentUser().getUid();
+    private ArrayList<String> friendsid = new ArrayList<>();
     private List<Friend> friends = new ArrayList<>();
     private  LinearLayoutManager layoutManager;
-    FriendsAdapter friendsAdapter;
-
+    private FriendsAdapter friendsAdapter;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Friend friend;
+    private CollectionReference mUserReference= db.collection("Users").document(
+            user_id).collection("friend"); private CollectionReference mCollectionRef = db.collection("Friends").document(
+            user_id).collection("friend");
 
 
     public Friends() {
@@ -49,6 +57,9 @@ public class Friends extends Fragment {
 
         }
 
+
+
+
     }
 
     @Override
@@ -56,19 +67,23 @@ public class Friends extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_friends, container, false);
 
+
+
+
         friends.add(new Friend("Jake", "22000"));
         friends.add(new Friend("Emily", "19500"));
 
 
-         /* for(Friend model : friends) {
-            System.out.println(model.getName());
-        } */
 
         recyclerView = view.findViewById(R.id.recyclerView_friends);
         layoutManager = new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
 
         friendsAdapter = new FriendsAdapter(friends);
+
+
+
+
         FriendsAdapter adapter = new FriendsAdapter(friends);
 
         //recyclerView.setAdapter(friendAdapter);
@@ -77,4 +92,56 @@ public class Friends extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+    }
+
+    private void getFriendsId(final mCallback mCallback){
+        mCollectionRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                    friend = snapshot.toObject(Friend.class);
+                    friendsid.add(snapshot.getId());
+                    Log.d("main", snapshot.getId());
+                }
+              mCallback.onCallback(friendsid);
+            }});
+
+    }
+
+
+
+
+    private void getFriends(){
+
+
+     getFriendsId(new mCallback() {
+         @Override
+         public void onCallback(ArrayList<String> list) {
+             Log.d("main44",list.toString());
+         }
+     });
+
+        CollectionReference mUserReference= db.collection("Users");
+
+
+
+
+    }
+
+    private interface mCallback{
+        void onCallback(ArrayList<String> list);
+}
+
+
+
+
+
+
+
 }
