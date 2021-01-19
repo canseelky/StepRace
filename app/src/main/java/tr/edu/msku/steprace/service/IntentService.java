13f7374df9fe56c1424f1cc80e7b797980f60d64
canselkucukyilmaz <canselhky@gmail.com>
2021-01-20 00:15:24 +0300

    Intent service updated

--- app/src/main/java/tr/edu/msku/steprace/service/IntentService.java
+++ app/src/main/java/tr/edu/msku/steprace/service/IntentService.java
@@ -6,41 +6,115 @@ import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class IntentService extends android.app.IntentService implements SensorEventListener {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-    private int numOfSteps;
-    private String userid;
-    private SharedPreferences mPref;
-    private SharedPreferences.Editor editor;
-    public IntentService(String name) {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import tr.edu.msku.steprace.model.Data;
+
+public class IntentService extends android.app.IntentService implements SensorEventListener {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String user_id = FirebaseAuth.getInstance().getCurrentUser().getUid().toString();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    String date = sdf.format(new Date()).toString();
+    private DocumentReference ref = db.collection("Data").document(user_id).collection("data").document(date);
+    private SensorManager manager;
+    private SensorManager mSensorManager;
+    private Sensor mStepDetectorSensor;
+
+
+
+    public IntentService() {
         super("Intent-Service");
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        mPref = getSharedPreferences("numOfStep", Context.MODE_PRIVATE);
+    protected void onHandleIntent(@Nullable Intent intent) {
     }
 
     @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
-        numOfSteps = 0 ;
-        userid = intent.getStringExtra("userid");
-
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+            mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+            mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            mSensorManager.registerListener(this, mStepDetectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            return super.onStartCommand(intent,flags,startId);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        editor = mPref.edit();
-        editor.putInt("numOfSteps",numOfSteps++);
-
+        databaseUpdate();
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+
+    private void databaseUpdate(){
+
+        ref.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Data data = new Data();
+                String step = documentSnapshot.getString("num");
+                Integer number_of_steps = Integer.valueOf(step);
+
+                if(documentSnapshot == null){
+
+                    data.setDate(date);
+                    data.setNum(1);
+
+                    ref.set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+
+                }
+
+                if (documentSnapshot.exists() && documentSnapshot != null && number_of_steps > 0 ){
+                    data.setDate(date);
+                    data.setNum(number_of_steps++);
+                    ref.set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+                }
+
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
 
     }
 }
