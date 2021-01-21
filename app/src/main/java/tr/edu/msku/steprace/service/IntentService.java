3f067a3416a109db14875082f89f6ff8cbb3b8aa
canselkucukyilmaz <canselhky@gmail.com>
2021-01-21 13:20:34 +0300

    Service updated

--- app/src/main/java/tr/edu/msku/steprace/service/IntentService.java
+++ app/src/main/java/tr/edu/msku/steprace/service/IntentService.java
@@ -2,37 +2,35 @@ package tr.edu.msku.steprace.service;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import tr.edu.msku.steprace.model.Data;
 
 public class IntentService extends android.app.IntentService implements SensorEventListener {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private String user_id = FirebaseAuth.getInstance().getCurrentUser().getUid().toString();
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-    String date = sdf.format(new Date()).toString();
-    private DocumentReference ref = db.collection("Data").document(user_id).collection("data").document(date);
     private SensorManager manager;
     private SensorManager mSensorManager;
     private Sensor mStepDetectorSensor;
+
+    private String TAG = getClass().getSimpleName();
+    private  SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    private String date = sdf.format(new Date()).toString();
+    private DocumentReference ref = db.collection("Data").document(user_id).collection("data").document(date);
+
 
 
 
@@ -49,7 +47,7 @@ public class IntentService extends android.app.IntentService implements SensorEv
             mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
             mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
             mSensorManager.registerListener(this, mStepDetectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
-            return super.onStartCommand(intent,flags,startId);
+            return START_STICKY;
     }
 
     @Override
@@ -71,51 +69,49 @@ public class IntentService extends android.app.IntentService implements SensorEv
 
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String n  = String.valueOf(documentSnapshot.get("num"));
+                int i= Integer.parseInt(n);
+                 Data data = new Data(date,i);
 
                 documentSnapshot.toObject(Data.class);
-                Data data = new Data();
-
                 //Integer step = documentSnapshot.getString("num");
-                int number_of_steps = Integer.valueOf(String.valueOf(data.getNum()));
-                Log.d("ıntent1" ,String.valueOf(data.getNum()));
-                if(number_of_steps == 0){
+                Log.d(TAG ,String.valueOf(data.getNum()));
+                if(data.getNum() == 0 ){
                     data.setDate(date);
-                    Log.d("ıntent1" ,String.valueOf(data.getNum()));
+                    Log.d(TAG ,String.valueOf(data.getNum()));
                     data.setNum(1);
-
                     ref.set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
                         public void onSuccess(Void aVoid) {
-                            Log.d("ıntent","database yazıldı");
+                            Log.d(TAG,"OK");
 
                         }
                     }).addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-                            Log.d("ıntent","database yazılamadı" + e.toString());
+                            Log.d(TAG,"Something went wrong.." + e.toString());
                         }
                     });
 
                 }
 
-                if (documentSnapshot.exists() && documentSnapshot != null && number_of_steps > 0 ){
-                    data.setDate(date);
-                    int numb = data.getNum();
-
-                    Log.d("ıntent1" ,String.valueOf(data.getNum()));
-                    data.setNum(numb++);
-                    Log.d("ıntent1" ,String.valueOf(data.getNum()));
-
+                if (documentSnapshot.exists() && documentSnapshot != null && data.getNum() > 0){
+                    //data.setDate(date);
+                        Log.d(TAG ,String.valueOf(data.getNum()));
+                    int mnumber = data.getNum()  + 1  ;
+                    //Log.d(TAG ,String.valueOf(mnumber));
+                    data.setNum(mnumber);
                     ref.set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
                         public void onSuccess(Void aVoid) {
-                            Log.d("ıntent","database yazıldı");
+
+                            Log.d(TAG,"OK");
 
                         }
                     }).addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-
+                            Log.d(TAG,  "Something went wrong.." + e.toString());
                         }
                     });
                 }
@@ -125,7 +121,7 @@ public class IntentService extends android.app.IntentService implements SensorEv
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-
+                Log.d("TAG","Something went wrong.." + e.toString());
             }
         });
 
